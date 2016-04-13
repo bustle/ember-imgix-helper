f5e0327c128c2ad48051cce6112cc09a68e7660b
James Yu <jj6yu@uwaterloo.ca>
2016-04-13 18:11:26 -0400

    GIFs are now processed by imgix correctly, this fix is no longer necessary

--- addon/helpers/imgix-src.js
+++ addon/helpers/imgix-src.js
@@ -13,21 +13,12 @@ function buildUrl(src, query) {
 var deviceDpr = Math.floor(window.devicePixelRatio) || 1;
 var dprParams = deviceDpr > 1 ? ('&dpr=' + deviceDpr) : '';
 
-export function isGif(src) {
-  return src ? !!src.match(/\.gif$|\.gif\?/) : false;
-}
-
 export function imgixSrc(helperParams, helperOptions) {
   var src         = helperParams[0];
   var options     = helperOptions       || {};
   var query       = options.query       || '';
   var processGifs = options.processGifs || false;
   var applyDpr    = options.applyDpr    || false;
-
-  // gifs become a single frame if you process them through imgix
-  if (!processGifs && isGif(src)) {
-    return src;
-  }
 
   if (applyDpr) {
     query += dprParams;
